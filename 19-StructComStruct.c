0693c20c43b202e39d563b5bd38817e90dd1a87e
Alison Krusciel de Moraes <78050333+alisonmoraes@users.noreply.github.com>
2022-04-26 23:47:16 -0300

    Exercícios C e C++
    Exercícios em c e c++

---
+++ 19-StructComStruct.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+typedef struct Data{
+    int dia;
+    int mes;
+    int ano;
+}Data;
+
+struct Aluno{
+    int id;
+    Data nascimento;
+};
+
+
+int main () {
+
+    //Criando um aluno
+    struct Aluno aluno1;
+
+    //Modificando valores
+    aluno1.id = 15;
+    aluno1.nascimento.ano = 1996;
+    aluno1.nascimento.mes = 1;
+    aluno1.nascimento.dia = 17;
+
+    //Imprimindo valores
+    printf("Nascido em: %d / %d / %d",  aluno1.nascimento.dia,  aluno1.nascimento.mes,  aluno1.nascimento.ano);
+
+    return 0;
+}
---
+++ 21-AulaEscreverEmArquivos.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <fstream>
+
+using namespace std;
+
+int main () {
+
+    //Cria o objeto de arquivo
+    ofstream arquivoDeSaida;
+
+    //Abre o arquivo ou cria caso não exista
+    arquivoDeSaida.open("Texto.txt", std::ios_base::app);
+
+    //Escreve algo
+    arquivoDeSaida << "\nMamãezinha";
+
+    //Fecha o arquivo
+    arquivoDeSaida.close();
+
+    return 0;
+}
+
+
